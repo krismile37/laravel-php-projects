f28e54a9e71ac2f2c8845097b5736671b2466b78
krismile37 <50942544+krismile37@users.noreply.github.com>
2024-10-26 18:07:03 +0500

    Add files via upload

---
+++ web_blog_reviews/storage/framework/views/2d9206e8ca8f0391f9982898654a566f.php
@@ -0,0 +1,10 @@
+
+
+<?php $__env->startSection('title'); ?>
+  Про нас
+<?php $__env->stopSection(); ?>
+
+<?php $__env->startSection('content'); ?>
+  <h1>Про нас</h1>
+<?php $__env->stopSection(); ?>
+<?php echo $__env->make('layout', \Illuminate\Support\Arr::except(get_defined_vars(), ['__data', '__path']))->render(); ?><?php /**PATH C:\xampp\htdocs\blog_reviews\resources\views/about.blade.php ENDPATH**/ ?>
