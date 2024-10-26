7c59516f764afb0d4a87caa5955aab816404379b
krismile37 <50942544+krismile37@users.noreply.github.com>
2024-10-26 18:11:07 +0500

    Add files via upload

---
+++ web_blog_reviews/resources/views/about.blade.php
@@ -0,0 +1,9 @@
+@extends('layout')
+
+@section('title')
+  Про нас
+@endsection
+
+@section('content')
+  <h1>Про нас</h1>
+@endsection
---
+++ web_blog_reviews/resources/views/home.blade.php
@@ -0,0 +1,9 @@
+@extends('layout')
+
+@section('title')
+  Главная
+@endsection
+
+@section('content')
+  <h1>Главная страница</h1>
+@endsection
